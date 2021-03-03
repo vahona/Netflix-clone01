db835032dee831602a1ea6cf1bfabdf7a719828c
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2021-03-03 07:38:29 +0300

    First try netflix clone

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Signin, Signup, Browse } from './pages';
+import {useAuthListener} from './hooks';
+import {IsUserRedirect, ProtectedRoute} from './helpers/routes'
+
+
+
+export function App() {
+	const {user} = useAuthListener()
+	console.log(user);
+	return (
+    <Router>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
+import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+// import { seedDatabase} from './seed'
+
+const config = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID ,
+};
+
+const firebase = window.firebase.initializeApp(config);
+
+// seedDatabase(firebase)
+
+ReactDOM.render(
+	<>
+		<FirebaseContext.Provider value={{ firebase: window.firebase }}>
+			<GlobalStyles />
+			<App />
+		</FirebaseContext.Provider>
+	</>,
+	document.getElementById('root')
+);
+
